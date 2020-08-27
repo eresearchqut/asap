f0e5f489684645085a7ea3861655bc4d0377a2bf
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-08-27 10:31:02 +1000

    Router as root url

--- asap/asap/urls.py
+++ asap/asap/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include
 from django.urls import re_path
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # Any custom URLs go here before we include the TRRF urls
+    re_path(r'^$', RedirectView.as_view(url='router/', permanent=False)),
     re_path(r'', include('rdrf.urls')),
 ]
